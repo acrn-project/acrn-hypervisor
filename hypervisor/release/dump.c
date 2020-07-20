333e198c28fd46231fa29a654ba424ed9016f405
Jie Deng <jie.deng@intel.com>
2020-07-20 22:47:00 +0800

    release: fix release compile error.
    Signed-off-by: Jie Deng <jie.deng@intel.com>

--- hypervisor/release/dump.c
+++ hypervisor/release/dump.c
@@ -9,3 +9,5 @@
 
 void dump_intr_excp_frame(__unused const struct intr_excp_ctx *ctx) {}
 void dump_exception(__unused struct intr_excp_ctx *ctx, __unused uint16_t pcpu_id) {}
+void debug_dump_host_state(void) {}
+void debug_dump_guest_cpu_regs(__unused struct acrn_vcpu *vcpu) {}
