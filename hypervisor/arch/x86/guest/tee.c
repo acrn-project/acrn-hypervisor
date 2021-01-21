03c0c5f55957bb1702867760563ec5f481aad4fd
Jie Deng <jie.deng@intel.com>
2021-01-21 14:33:29 +0800

    hv: tee: add shared memory for TEE/REE
    GPA from (4G -2M) ~ 4G are shared by TEE/REE.
    The TEE/REE can access this GPA for the same
    HPA.
    
    Signed-off-by: Jie Deng <jie.deng@intel.com>

--- hypervisor/arch/x86/guest/tee.c
+++ hypervisor/arch/x86/guest/tee.c
@@ -8,6 +8,8 @@
 #include <vm_config.h>
 #include <tee.h>
 
+uint8_t tee_smc_shared_mem[TEE_SMC_CALL_SHARED_PAGE_SIZE];
+
 bool is_tee_vm(const struct acrn_vm *vm)
 {
 	return (vm != NULL) && (vm->sw.kernel_type == KERNEL_TEE);
