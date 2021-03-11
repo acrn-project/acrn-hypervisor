f6e924bfbfd9fca1bc7d867366c3e4fdafff16ea
Jie Deng <jie.deng@intel.com>
2021-03-11 23:30:35 +0800

    using a dedicate lock
    Signed-off-by: Jie Deng <jie.deng@intel.com>

--- hypervisor/arch/x86/guest/vm.c
+++ hypervisor/arch/x86/guest/vm.c
@@ -1031,3 +1031,14 @@ void put_vm_lock(struct acrn_vm *vm)
 {
 	spinlock_release(&vm->vm_state_lock);
 }
+
+void get_split_lock(struct acrn_vm *vm)
+{
+	spinlock_obtain(&vm->split_lock);
+}
+
+void put_split_lock(struct acrn_vm *vm)
+{
+	spinlock_release(&vm->split_lock);
+}
+
